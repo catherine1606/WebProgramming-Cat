b089d0b6a42e871ecb08fa9b73f8de9bfb517458
fifi0327 <mnbv90327@gmail.com>
2022-05-19 16:53:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+//這個function的功能是會自動偵測現在頁面，並讓header的標籤呈現灰底。
 function detect(){
-	$('li').each(function(index){
-		if($(this).text() == "首頁"){
-			$(this).css('background','#ccc');
-		}
+	if($('title').text() == "首頁")
+	{
+		$('li').each(function(index)
+		{
+			if($(this).text() == "首頁")
+			{
+				$(this).css('background','#f0f0f0');
+			}
+		});
+	}
+	if($('title').text() == "領養")
+	{
+		$('li').each(function(index)
+		{
+			if($(this).text() == "領養")
+			{
+				$(this).css('background','#f0f0f0');
+			}
+		});
+	}
+	if($('title').text() == "購物")
+	{
+		$('li').each(function(index)
+		{
+			if($(this).text() == "購物")
+			{
+				$(this).css('background','#f0f0f0');
+			}
+		});
+	}
+	if($('title').text() == "小遊戲")
+	{
+		$('li').each(function(index)
+		{
+			if($(this).text() == "小遊戲")
+			{
+				$(this).css('background','#f0f0f0');
+			}
+		});
 	}
 }
